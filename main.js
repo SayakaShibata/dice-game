69b672acda12035c3f36bc78eb950f68b0adb8eb
SayakaShibata <66930033+SayakaShibata@users.noreply.github.com>
2020-06-15 15:29:59 +0900

    chenge

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 //roll Dice
-
 function roll1 (){
   var die1 = Math.floor(Math.random( )* 6) ;
   var die3 = Math.floor(Math.random( )* 6) ;
   var a1= [0,64,128,192,256,320]
   var a3= [0,64,128,192,256,320]
+  var c1= [1,2,3,4,5,6]
+  var c3= [1,2,3,4,5,6]
   document.getElementById('dice1').style.backgroundPosition = '-'+a1[die1]+'px -128px';
   document.getElementById('dice3').style.backgroundPosition = '-'+a3[die3]+'px -128px';
-  var player1 = parseInt(die1 + die3);
+  var player1 = parseInt(c1[die1] + c3[die3]);
   document.getElementById('p3').innerHTML= 'total = '+ player1;
+
+  var html1 =  '<ul class="list-group">';
+for (var n=0; n< player1.length; n++){   
+      html1 += `<li class="list">`+player1[n]+`</li>`;
+    }
+      html1+=`</ul>`
+  document.getElementById(`log1`).innerHTML=html1
+
   men1=player1;
   dice1=die1;
   dice3=die3;
@@ -19,10 +28,19 @@ function roll2 (){
   var die4 = Math.floor(Math.random( )* 6) ;
   var a2= [0,64,128,192,256,320]
   var a4= [0,64,128,192,256,320]
+  var c2= [1,2,3,4,5,6]
+  var c4= [1,2,3,4,5,6]
   document.getElementById('dice2').style.backgroundPosition = '-'+a2[die2]+'px -320px';
   document.getElementById('dice4').style.backgroundPosition = '-'+a4[die4]+'px -320px';
-  var player2 = parseInt(die2 + die4);
+  var player2 = parseInt(c2[die2] + c4[die4]);
   document.getElementById('p4').innerHTML= 'total = '+ player2;
+  
+  var html2 =  '<ul class="list-group">';
+for (var f=0; f< player2.length; f++){   
+      html2 += `<li class="list">`+player2[f]+`</li>`;
+    }
+      html2+=`</ul>`
+  document.getElementById(`log2`).innerHTML=html2
   men2=player2;
   dice2=die2;
   dice4=die4;
@@ -35,14 +53,11 @@ function roll2 (){
   'dic2': 0
 }
 ]
-function rollcount() {
-  roll1();
-  roll2();
- document.getElementById('p3').innerHTML= 'total'+ men1;
- document.getElementById('p4').innerHTML= 'total'+ men2;
+
+function roll() {
  if(men1>men2) document.getElementById('winner').innerHTML='player 1 is win';
  if(men1<men2) document.getElementById('winner').innerHTML='player 2 is win';
- if(men1===men2) document.getElementById('winner').innerHTML='one more try'; 
+ if(men1===men2) document.getElementById('winner').innerHTML='- It is TEI -'; 
 
   var sum1 = 0;
   var sum2 = 0;
@@ -67,7 +82,6 @@ for (var f=0; f< counts.length; f++){
  sums1=sum1;
  sums2=sum2;
 }
-
   if (sum1 ==5 || sum2 ==5){
     openModal() ;
    } else {
@@ -80,6 +94,12 @@ for (var f=0; f< counts.length; f++){
       }
     }
   }
+}
+//Key
+function rollcount(){
+  roll1();
+  roll2();
+  roll();
 }
 
 //show model
